5b0b2b966b49434ea8de5ac6bcb88b0705d8e022
Ad-Astra-Hackathon-Team <134006933+Ad-Astra-Hackathon-Team@users.noreply.github.com>
2023-05-21 10:46:10 +0300

    Add files via upload

---
+++ Installations (1).py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+get_ipython().system('pip install pickle')
+get_ipython().system('pip install numpy')
+get_ipython().system('pip install torch torchvision torchaudio')
+get_ipython().system('pip install os')
+get_ipython().system('pip install pandas')
+get_ipython().system('pip install tkinter')
+
+get_ipython().system('pip install tqdm')
+get_ipython().system('pip install PIL')
+get_ipython().system('pip install pathlib')
+
+get_ipython().system('pip install multiprocessing')
+get_ipython().system('pip install sklearn')
+get_ipython().system('pip install matplotlib')
+
+get_ipython().system('pip install warnings')
+
